02697ab23241d77a0aae6b0fb9500728a77b449f
Ben Swanson <benswanson127@gmail.com>
2017-06-25 16:35:26 -0600

    Added autospawn for upgraders

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var roleBuilder = require('role.builder');
 module.exports.loop = function () {
     
     
-    //Auto Spawn Harvesters
+    //Auto Delete unused
         for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -17,13 +17,20 @@ module.exports.loop = function () {
         }
     }
 
+    //Variables for creeps
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     
-
+    //Auto Spawn
     if(harvesters.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
+    else if(upgraders.length < 2) { 
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
+    }
+    
 
     if(Game.spawns['Spawn1'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
@@ -33,10 +40,26 @@ module.exports.loop = function () {
             Game.spawns['Spawn1'].pos.y,
             {align: 'left', opacity: 0.8});
     }
-    // End Auto Spawn Harvesters
+    // End Auto Spawn
     
     
     
+    
+    //Main Loop to run roles
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
     
     //Main Loop to run roles
 
